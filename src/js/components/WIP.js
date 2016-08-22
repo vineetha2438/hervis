241663d030f1539ecbc66da10b6cfb1b720768d4
K Vineetha <k_vineetha@mckinsey.com>
2016-08-22 11:54:23 +0530

    work in progress page added

---
+++ src/js/components/WIP.js
@@ -0,0 +1,20 @@
+/**
+ * Created by kvineetha on 8/19/16.
+ */
+
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import Dimensions from 'react-dimensions';
+import './../../styles/wip.css';
+
+export default class WIP extends Component {
+  render() {
+    return (
+      <div className="wipContainer">
+        <img className="logo" src={'./../../images/Hervis-logo-white.png'} alt="logo" />
+        <img className="constructionImage" src={'./../../images/main.png'} alt="construction" />
+      </div>
+    );
+  }
+}
+
